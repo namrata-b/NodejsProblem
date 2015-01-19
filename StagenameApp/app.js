04bb3ea88d1df87d7da0af7acf52e91579f50301
Namrata Bandekar <namrata.bandekar@gmail.com>
2015-01-19 18:23:08 -0500

    Add missing functionality for fizzbuzzwoof.
    Signed-off-by: Namrata Bandekar <namrata.bandekar@gmail.com>

--- StagenameApp/app.js
+++ StagenameApp/app.js
@@ -1,14 +1,14 @@
 var express = require('express')
 var app = express()
 
-// get fizzbuzz
+// respond with fizzbuzz output for GET at /fizzbuzz/:foo
 app.get('/fizzbuzz/:foo', function(req, res) {
 	var myInstance = new FizzbuzzClass();
 	var ret = myInstance.fizzbuzz(req.params.foo);
 	res.send(ret);
 })
 
-// get fizzbuzzwoof
+// respond with fizzbuzzwoof output for GET at /fizzbuzzwoof/:foo
 app.get('/fizzbuzzwoof/:foo', function(req, res) {
 	var myInstance = new FizzbuzzClass();
 	var ret = myInstance.fizzbuzzwoof(req.params.foo);
@@ -45,6 +45,11 @@ FizzbuzzClass.prototype.fizzbuzzwoof = function(foo) {
 	if (foo % 15 == 0) {
 		return "woof!";
 	};
-	
+	if (foo % 5 == 0) {
+		return "buzz";
+	};
+	if (foo % 3 == 0) {
+		return "fizz";
+	};
 	return foo;
 };
