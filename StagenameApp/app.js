a0a30092374d91848d9284584e0fb81c48193da3
Namrata Bandekar <namrata.bandekar@gmail.com>
2015-01-19 18:14:54 -0500

    Implement fizzbuzzwoof.
    Signed-off-by: Namrata Bandekar <namrata.bandekar@gmail.com>

--- StagenameApp/app.js
+++ StagenameApp/app.js
@@ -9,7 +9,11 @@ app.get('/fizzbuzz/:foo', function(req, res) {
 })
 
 // get fizzbuzzwoof
-
+app.get('/fizzbuzzwoof/:foo', function(req, res) {
+	var myInstance = new FizzbuzzClass();
+	var ret = myInstance.fizzbuzzwoof(req.params.foo);
+	res.send(ret);
+})
 
 var server = app.listen(8000, function () {
 
@@ -34,5 +38,13 @@ FizzbuzzClass.prototype.fizzbuzz = function(foo) {
 	if (foo % 3 == 0) {
 		return "fizz";
 	};
+	return foo;
+};
+
+FizzbuzzClass.prototype.fizzbuzzwoof = function(foo) {
+	if (foo % 15 == 0) {
+		return "woof!";
+	};
+	
 	return foo;
 };
