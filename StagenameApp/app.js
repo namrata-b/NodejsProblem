1f6c2e78bcdd18a6101aee809c034c56cd3d9e04
Namrata Bandekar <namrata.bandekar@gmail.com>
2015-01-19 18:06:36 -0500

    Code cleanup.
    Signed-off-by: Namrata Bandekar <namrata.bandekar@gmail.com>

--- StagenameApp/app.js
+++ StagenameApp/app.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var app = express()
 
-// get
+// get fizzbuzz
 app.get('/fizzbuzz/:foo', function(req, res) {
-	var myInstance = new FizbuzzClass();
-	var ret = myInstance.fizbuzz(req.params.foo);
+	var myInstance = new FizzbuzzClass();
+	var ret = myInstance.fizzbuzz(req.params.foo);
 	res.send(ret);
 })
+
+// get fizzbuzzwoof
+
 
 var server = app.listen(8000, function () {
 
@@ -17,11 +20,11 @@ var server = app.listen(8000, function () {
 
 })
 
-function FizbuzzClass (){
+function FizzbuzzClass (){
 
 }
 
-FizbuzzClass.prototype.fizbuzz = function(foo) {
+FizzbuzzClass.prototype.fizzbuzz = function(foo) {
 	if (foo % 15 == 0) {
 		return "fizzbuzz";
 	};
@@ -33,16 +36,3 @@ FizbuzzClass.prototype.fizbuzz = function(foo) {
 	};
 	return foo;
 };
-// static function fizbuzz(foo) {
-
-// 	if (foo % 15 == 0) {
-// 		return "fizzbuzz";
-// 	};
-// 	if (foo % 5 == 0) {
-// 		return "buzz";
-// 	};
-// 	if (foo % 3 == 0) {
-// 		return "fizz";
-// 	};
-// 	return foo;
-// }
